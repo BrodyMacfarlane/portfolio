e2ced39f37d460382b7df5c6357dd6e4e383f65b
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-12-27 12:51:14 -0700

    final draft

--- src/App.js
+++ src/App.js
@@ -19,7 +19,17 @@ class App extends Component {
       <HashRouter>
         <div className="App">
           <div className="navBar">
-            <Link to="/about"><h1 className="title">BM</h1></Link>
+            <Link to="/about">
+              <div className="title">
+                <h1 className="title-text">BM</h1>
+                <svg className="da-svg" width="100" height="100">
+                  <line className="top-ani" x1="100" y1="0" x2="-200" y2="0" />
+                  <line className="right-ani" x1="100" y1="100" x2="100" y2="-200" />
+                  <line className="bot-ani" x1="0" y1="100" x2="300" y2="100" />
+                  <line className="left-ani" x1="0" y1="0" x2="0" y2="300" />
+                </svg>
+              </div>
+            </Link>
             <div className="right-nav">
               <div className="work"><Link id={this.state.comp === "work" ? "onComp" : null} to="/work">WORK</Link></div>
               <div className="about"><Link id={this.state.comp === "about" ? "onComp" : null} to="/about">ABOUT</Link></div>
--- src/components/about.js
+++ src/components/about.js
@@ -13,9 +13,9 @@ export default class About extends Component {
         return (
             <div className="component-container">
                 <img className="code-pic about-content" src={code}/>
-                <div className="about-content">
-                    <h2>About</h2>
-                    <p>I am a web developer/designer who is very passionate about aesthetic, animation, and code hygiene.  I love learning new things, and enjoy tackling new challenges and venturing into rougher seas.  I love building knowledge of new frameworks and libraries, and adapting to the ever-changing industry of software development.</p>
+                <div className="about-content about-text">
+                    <h2 className="about-about">ABOUT</h2>
+                    <p>I am a web developer/designer who is very passionate about aesthetic, animation, and code hygiene.  I love learning new technologies, innovating ways to conquer real-world problems, and enjoy tackling challenges and venturing into rougher seas.  I love building knowledge of frameworks and libraries, and adapting to the ever-changing industry of software development.</p>
                 </div>
             </div>
         )
