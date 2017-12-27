6748a375679a066c32d39426136e7c0d2c7e8094
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-12-27 13:36:49 -0700

    favicon

--- src/components/work.js
+++ src/components/work.js
@@ -14,12 +14,15 @@ export default class Work extends Component {
                 <div className="projects">
                     <a href="http://dategen.io">
                         <div className="project date-project">DG</div>
+                        <div className="project-title">DATE GEN</div>
                     </a>
                     <a href="http://divemusic.io">
                         <div className="project dive-project">DV</div>
+                        <div className="project-title">DIVE MUSIC</div>
                     </a>
                     <a href="http://github.com/BrodyMacfarlane/sky-weather">
                         <div className="project sky-project">SK</div>
+                        <div className="project-title">SKY WEATHER</div>
                     </a>
                 </div>
             </div>
