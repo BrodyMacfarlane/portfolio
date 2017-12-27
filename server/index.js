d96ad3b6aaa0f8de0b7f0caaf896446769f151c2
Brody Macfarlane <macfarlanebrody@gmail.com>
2017-12-27 15:39:02 -0700

    pathing with browserrouter

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use( express.static( `${__dirname}/../build` ) );
 app.use(bodyParser.json())
 app.use(cors())
 
-// const path = require('path')
-// app.get('*', (req, res)=>{
-//   res.sendFile(path.join(__dirname, '/../build/index.html'));
-// })
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/../build/index.html'));
+})
 
 
 app.listen(9001, console.log("Listening on port 9001"))
